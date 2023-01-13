b744b1d93d047d832c4a6645d8990ea4f92bb70f
sageerkv <sageerkv93@gmail.com>
2023-01-13 12:26:32 +0530

    first commit

---
+++ landing-page/script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.th').src = anything;
+}
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+} 
