b8b9ccdfac619dbacd538305df19c43c7d7e3aac
sageerkv <sageerkv93@gmail.com>
2023-01-31 11:56:40 +0530

    initial commit

--- landing-page/script.js
+++
@@ -1,7 +0,0 @@
-function imgSlider(anything){
-    document.querySelector('.th').src = anything;
-}
-function changeBgColor(color){
-    const sec = document.querySelector('.sec');
-    sec.style.background = color;
-} 
